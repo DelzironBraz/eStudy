d16041fe85a9f950dbdd576060f05fbbdf936acd
DelzironBraz <delziron.braz@hotmail.com>
2023-12-23 14:14:14 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+    </main>
+  );
+};
+
+export default App;
