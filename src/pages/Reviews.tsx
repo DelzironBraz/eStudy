fad6c46c2fff837cc3ae908b900f69e3c5afc460
DelzironBraz <delziron.braz@hotmail.com>
2023-12-23 19:16:00 -0300

    style: fix navbar and reviews padding

--- src/pages/Reviews.tsx
+++ src/pages/Reviews.tsx
@@ -4,7 +4,7 @@ import { ReviewsData } from "../utils/reviews-data";
 
 const Reviews = () => {
     return (
-        <section className="min-h-screen flex flex-col items-center justify-center md:px-32 px5">
+        <section className="min-h-screen flex flex-col items-center justify-center md:px-32 px-5">
             <Heading title="Our" subTitle="Reviews" />
 
             <div className="flex flex-col md:flex-row gap-5 mt-5">
